e760296fa4504177ddfda4b7a7f1c2738e6efca5
Ayush Kumar <rajaayush124212@gmail.com>
2025-01-08 14:26:50 +0530

    initial connit

---
+++ Basics/numbeRprinteR.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class numbeRprinteR {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the last number");
+int num=1;
+int number=sc.nextInt();
+System.out.println("Here are the numbers: ");
+while (num<=number) {
+    System.out.println(num);
+    num++;
+}
+}   
+}
