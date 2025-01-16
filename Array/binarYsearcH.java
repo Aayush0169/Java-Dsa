2c569d694fe559f8999fcd7c7a9e9b63fc4a37c0
Ayush Kumar <rajaayush124212@gmail.com>
2025-01-16 18:48:38 +0530

    initial commit

---
+++ Array/binarYsearcH.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class binarYsearcH {
+    public static int BinarYsearcH(int nums[],int key){
+        int start=0;
+        int end=nums.length-1;
+        while (start<=end) {
+            int mid=(start+end)/2;
+            if (nums[mid]==key) {
+                return mid;
+            }
+             if(nums[mid]<key){
+                 start=mid+1;
+                }
+                else  {
+                    end=mid-1;
+                    
+                }      
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter he value of key you want to search in array: ");
+        int key=sc.nextInt();
+        int nums[]={8,4,12,15,16,18,20,24,30};
+        int result=BinarYsearcH(nums,key);
+        if (result>=0) {
+            System.out.println("Key found at index: "+result);
+        }
+        else{
+            System.out.println("couldnt find key!");
+        }
+        sc.close();
+    }
+}
