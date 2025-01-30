1f18792c7d41ae43c6eba431f698dab2e9ef420d
Ayush Kumar <rajaayush124212@gmail.com>
2025-01-30 09:35:13 +0530

    initial commit

--- Array/pairs.java
+++ Array/pairs.java
@@ -1,6 +1,6 @@
 public class pairs {
     int total=0;
-    public static void revrse(int arr[]){
+    public static void pairs(int arr[]){
         for(int i=0;i<arr.length;i++){
             int curr=arr[i];
             for(int j=i+1;j<arr.length;j++){
@@ -11,6 +11,7 @@ public class pairs {
     }
     public static void main(String[] args) {
         int nums[]={2,5,4,3,6,9,8};
-        revrse(nums);
+        System.out.println("Pairs in the array:");
+        pairs(nums);
     }
 }
