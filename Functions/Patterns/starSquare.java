1ee5dec3d2d6be89b9f30022c31afdf75f2e6f31
Ayush Kumar <rajaayush124212@gmail.com>
2025-02-04 10:06:46 +0530

    initial commit

--- Functions/Patterns/starSquare.java
+++ Functions/Patterns/starSquare.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class starSquare {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter number of rows");
-        int n=sc.nextInt();
+    public static void sqrOfStar(int n){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int n=sc.nextInt();
+        System.out.println("Here is your pattern of "+ n +" rows:");
+        sqrOfStar(n);
         sc.close();
     }
 }
