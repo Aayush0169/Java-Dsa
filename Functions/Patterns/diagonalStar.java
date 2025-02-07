c9605762c57ca89f9673c69ddb44d2c3ac3c43d3
Ayush Kumar <rajaayush124212@gmail.com>
2025-02-07 18:46:10 +0530

    initial commit

---
+++ Functions/Patterns/diagonalStar.java
@@ -0,0 +1,17 @@
+public class diagonalStar {
+    public static void main(String[] args) {
+        int n = 5; // Size of the grid
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  "); 
+                }
+            }
+            System.out.println(); 
+        }
+    }
+}
+
