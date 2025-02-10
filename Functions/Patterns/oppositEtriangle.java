a8c18dbcf9a8c0fcec7917dc87227c56c79a8b0f
Ayush Kumar <rajaayush124212@gmail.com>
2025-02-10 18:44:32 +0530

    updated another method

--- Functions/Patterns/oppositEtriangle.java
+++ Functions/Patterns/oppositEtriangle.java
@@ -1,10 +1,31 @@
 import java.util.Scanner;
 
 public class oppositEtriangle {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
-        int rows=sc.nextInt();
+    public static void oppositEtriangle1(int rows){
+        int lines=rows;
+        int space=rows-1;
+        int star=1;
+        int current_line=1;
+        while (current_line<=lines) {
+            //print spaces
+            for(int i=1;i<=space;i++){
+                System.out.print(" ");
+            }
+
+            //print stars
+            for(int i=1;i<=star;i++){
+                System.out.print("*");
+            }
+
+            //next line
+            System.out.println();
+            current_line++;
+            space--;
+            star++;
+        }
+    }
+
+    public static void oppositEtriangle2(int rows){//second method
         for(int row=1;row<=rows;row++){
             for(int col=1;col<=rows;col++){
                 if (row+col>rows) {
@@ -16,7 +37,15 @@ public class oppositEtriangle {
                 }
             }
             System.out.println();
-        }
+    }
+}
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows=sc.nextInt();
+        oppositEtriangle1(rows);
+        oppositEtriangle2(rows);
+
         sc.close();
     }
 }
