fe946ca92bdf678c2b4e87035e7502fe79bf5f41
Ayush Kumar <rajaayush124212@gmail.com>
2025-04-09 09:57:57 +0530

    initial commit

---
+++ Strings/basic.java
@@ -0,0 +1,16 @@
+public class basic {
+    public static void main(String[] args) {
+        String a="hi";
+        String b= "hi";
+        String alphabet="";
+        System.out.println('v'+0);
+
+        for(int i=0;i<26;i++) {
+            char ch = (char)('a'+i);
+            System.out.print(ch+" ");
+            alphabet+=ch;
+        }
+        System.out.println();
+       System.out.println(alphabet);
+    }
+}
